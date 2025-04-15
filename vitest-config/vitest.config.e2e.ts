321b1376700390e1da7afce63c05c62260874f76
denis-st-woof-software <denis.st@woof.software>
2025-04-15 20:16:36 +0300

    [86c339r58] vitest config add name

--- vitest-config/vitest.config.e2e.ts
+++ vitest-config/vitest.config.e2e.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
+        name: 'e2e',
         include: ['packages/**/test/e2e/**/*.test.ts'],
         environment: 'node',
     },
--- vitest-config/vitest.config.local.ts
+++ vitest-config/vitest.config.local.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
+        name: 'local',
         include: ['packages/**/test/local/**/*.test.ts'],
         environment: 'node',
     },
