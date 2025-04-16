e24c8b167606c0e4e6cb060986b9de4e10ee3598
glebgolyan <glebholian@gmail.com>
2025-04-16 10:09:08 +0300

    small change

--- packages/comet-sdk-wagmi/src/utils/wagmi.utils.ts
+++ packages/comet-sdk-wagmi/src/utils/wagmi.utils.ts
@@ -2,7 +2,7 @@ import type { MulticallReturnType } from "@wagmi/core";
 
 export class WagmiUtils {
   static resultOrThrow<T>(response?: MulticallReturnType[number]) {
-    if (response?.result === undefined || response.error || !response)
+    if (!response || response.error || !response)
       throw response?.error || new Error("Failed to get multicall result");
     return response.result as T;
   }
