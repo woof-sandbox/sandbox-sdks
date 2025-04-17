9dd156842b72ae69fbb5f0f5bf83a76430d3815f
denis-st-woof-software <denis.st@woof.software>
2025-04-17 13:36:49 +0300

    [86c339r58] add all tests

---
+++ packages/contracts-tools-sdk-ethers/test/vitest-config/vitest.config.all.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        name: 'all',
+        include: [
+            'test/unit/**/*.test.ts',
+            'test/e2e/**/*.e2e-test.ts',
+            'test/local/**/*.local-test.ts'
+        ],
+        environment: 'node',
+    },
+})
