59f2c37aea4e641e2f280c1aab6b7ae68bc73409
denis-st-woof-software <denis.st@woof.software>
2025-04-17 13:20:19 +0300

    [86c339r58] rename test

--- packages/contracts-tools-sdk-ethers/test/vitest-config/vitest.config.local.ts
+++ packages/contracts-tools-sdk-ethers/test/vitest-config/vitest.config.local.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         name: 'local',
-        include: ['test/local/**/*.test.ts'],
+        include: ['test/local/**/*.local-test.ts'],
         environment: 'node',
     },
 })
