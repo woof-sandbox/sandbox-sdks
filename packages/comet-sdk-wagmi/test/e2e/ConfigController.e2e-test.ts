8bfb2ca3929909d50ccc7acf6bedc6d765ef0438
glebgolyan <glebholian@gmail.com>
2025-04-21 16:32:19 +0300

    fixed tests

--- packages/comet-sdk-wagmi/test/e2e/ConfigController.e2e-test.ts
+++ packages/comet-sdk-wagmi/test/e2e/ConfigController.e2e-test.ts
@@ -1,14 +1,18 @@
 import { beforeAll, describe, expect, test } from "vitest";
 import { Chain } from "../../src/config/chains";
-import { ConfigControllerWrapper } from "../../src/wrapper/ConfigControllerWrapper";
 import { ConfigController } from "../../src/augment/ConfigController";
+import { wagmiConfig } from "../../lib/contracts";
 
 const configController = "0xDF539a3B60172779Be6cBa11B26bBE0913b5316A";
 let configC: ConfigController;
 
 describe("Configurato Contract", () => {
   beforeAll(async () => {
-    configC = await ConfigController.fetch(configController, Chain.Sepolia);
+    configC = await ConfigController.fetch(
+      configController,
+      Chain.Sepolia,
+      wagmiConfig,
+    );
   });
 
   test("curator", async () => {
