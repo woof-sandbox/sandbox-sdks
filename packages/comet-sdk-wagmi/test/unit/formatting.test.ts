934ff733f48a8556853c0c54d536e28d01ff8510
denis-st-woof-software <denis.st@woof.software>
2025-04-30 19:28:55 +0300

    [86c2tefkm] fix formatting.test

--- packages/comet-sdk-wagmi/test/unit/formatting.test.ts
+++ packages/comet-sdk-wagmi/test/unit/formatting.test.ts
@@ -3,14 +3,14 @@ import { FormattingUtils } from "../../src/utils";
 
 describe("FormattingUtils", () => {
   test("formatTokenValue", () => {
-    expect(FormattingUtils.formatTokenValue(1000n, 3)).toBe("1.0");
+    expect(FormattingUtils.formatTokenValue(1000n, 3)).toBe("1");
     expect(FormattingUtils.formatTokenValue(1234567890123456789n, 18)).toBe(
       "1.234567890123456789",
     );
     expect(FormattingUtils.formatTokenValue(5000000000000000000n, 18)).toBe(
-      "5.0",
+      "5",
     );
-    expect(FormattingUtils.formatTokenValue(1000000000n, 6)).toBe("1000.0");
+    expect(FormattingUtils.formatTokenValue(1000000000n, 6)).toBe("1000");
   });
 
   test("formatNumber", () => {
