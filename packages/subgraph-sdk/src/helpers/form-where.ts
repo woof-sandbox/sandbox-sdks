6c01950edd9873306cbc63ea2e69960a87224018
Michael Hasso <misha.h@woof.software>
2025-06-03 19:27:04 +0200

    add form helpers to subgraph sdk

--- packages/subgraph-sdk/src/helpers/form-where.ts
+++ packages/subgraph-sdk/src/helpers/form-where.ts
@@ -3,11 +3,13 @@ export const formWhere = (
 ): string => {
   let where = "";
   if (Object.keys(whereFilters).length) {
-    where += " { ";
+    where += "{ ";
 
     for (const [key, val] of Object.entries(whereFilters)) {
       if (Array.isArray(val)) {
         where += `${key}: [${val.map((v) => `"${v}"`).join(", ")}]`;
+      } else if (val.startsWith("{") && val.endsWith("}")) {
+        where += `${key}: ${val}`;
       } else {
         where += `${key}: "${val}"`;
       }
