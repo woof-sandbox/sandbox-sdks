42e194ab228eb10406750b803f8de5008c209027
Stanislav Baraniuk <stas.b@woof.software>
2026-01-07 18:43:42 +0100

    fix: hardcode ankr RPC

--- packages/comet-sdk-wagmi/src/contracts/wagmiConfig.ts
+++ packages/comet-sdk-wagmi/src/contracts/wagmiConfig.ts
@@ -5,8 +5,8 @@ import { WagmiChains } from "../config";
 export const wagmiConfig = createConfig({
   chains: WagmiChains,
   transports: {
-    [mainnet.id]: http(),
-    [arbitrum.id]: http(),
-    [sepolia.id]: http(),
+    [mainnet.id]: http("https://rpc.ankr.com/eth/e1d0a2568ebc983c815a7a7190b3d2bb39d859b8186ae0dd32a4354f71601591"),
+    [arbitrum.id]: http("https://rpc.ankr.com/arbitrum/e1d0a2568ebc983c815a7a7190b3d2bb39d859b8186ae0dd32a4354f71601591"),
+    [sepolia.id]: http("https://rpc.ankr.com/eth_sepolia/e1d0a2568ebc983c815a7a7190b3d2bb39d859b8186ae0dd32a4354f71601591"),
   },
 });
