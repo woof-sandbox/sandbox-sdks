989d267a255c2734bfb9355396bdaaec281d99b3
Stanislav Baraniuk <stas.b@woof.software>
2026-01-29 16:04:13 +0100

    feat(sandbox): replace sepolia id type with temporary id

--- packages/comet-sdk-wagmi/src/config/chains/wagmi.chains.ts
+++ packages/comet-sdk-wagmi/src/config/chains/wagmi.chains.ts
@@ -3,4 +3,5 @@ import { arbitrum, mainnet, sepolia } from "@wagmi/core/chains";
 export const WagmiChains = [mainnet, sepolia, arbitrum] as const;
 export const WagmiChainIds = [mainnet.id, sepolia.id, arbitrum.id] as const;
 
-export type WagmiChainId = (typeof WagmiChainIds)[number];
+export type WagmiChainId = 1 | 42161 | 111555111;
+// export type WagmiChainId = (typeof WagmiChainIds)[number];
