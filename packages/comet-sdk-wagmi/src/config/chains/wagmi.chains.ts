13c882155bf18b94a525eb97053053090dfdd60d
Stanislav Baraniuk <stas.b@woof.software>
2026-01-29 16:14:14 +0100

    feat(sandbox): update id spoofing approach

--- packages/comet-sdk-wagmi/src/config/chains/wagmi.chains.ts
+++ packages/comet-sdk-wagmi/src/config/chains/wagmi.chains.ts
@@ -1,7 +1,11 @@
 import { arbitrum, mainnet, sepolia } from "@wagmi/core/chains";
 
-export const WagmiChains = [mainnet, sepolia, arbitrum] as const;
-export const WagmiChainIds = [mainnet.id, sepolia.id, arbitrum.id] as const;
+export const fakeSepolia = {
+  ...sepolia,
+  id: 111555111 as const,
+}
 
-export type WagmiChainId = 1 | 42161 | 111555111;
-// export type WagmiChainId = (typeof WagmiChainIds)[number];
+export const WagmiChains = [mainnet, fakeSepolia, arbitrum] as const;
+export const WagmiChainIds = [mainnet.id, fakeSepolia.id, arbitrum.id] as const;
+
+export type WagmiChainId = (typeof WagmiChainIds)[number];
--- packages/comet-sdk-wagmi/src/contracts/wagmiConfig.ts
+++ packages/comet-sdk-wagmi/src/contracts/wagmiConfig.ts
@@ -1,12 +1,13 @@
 import { http, createConfig } from "@wagmi/core";
 import { arbitrum, mainnet, sepolia } from "@wagmi/core/chains";
-import { WagmiChains } from "../config";
+import {fakeSepolia, WagmiChains} from "../config";
 
 export const wagmiConfig = createConfig({
   chains: WagmiChains,
   transports: {
     [mainnet.id]: http("https://rpc.ankr.com/eth/e1d0a2568ebc983c815a7a7190b3d2bb39d859b8186ae0dd32a4354f71601591"),
     [arbitrum.id]: http("https://rpc.ankr.com/arbitrum/e1d0a2568ebc983c815a7a7190b3d2bb39d859b8186ae0dd32a4354f71601591"),
-    [sepolia.id]: http("https://rpc.ankr.com/eth_sepolia/e1d0a2568ebc983c815a7a7190b3d2bb39d859b8186ae0dd32a4354f71601591"),
+    // [sepolia.id]: http("https://rpc.ankr.com/eth_sepolia/e1d0a2568ebc983c815a7a7190b3d2bb39d859b8186ae0dd32a4354f71601591"),
+    [fakeSepolia.id]: http("https://virtual.sepolia.eu.rpc.tenderly.co/be13bd77-e574-49da-86c9-e3d2422cea28"),
   },
 });
